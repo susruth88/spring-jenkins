f52f03e7f9e740948f19bb484f972c223118d765
susruth08 <susruth08@gmail.com>
2023-12-13 15:26:30 -0500

    init

--- src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
+++ src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
@@ -1,13 +1,19 @@
 package com.javatechie.jenkin.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 @RestController
 public class SpringJenkinsApplication {
+
+	public static Logger logger  = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 
 
 	@GetMapping("/ping")
@@ -15,7 +21,14 @@ public class SpringJenkinsApplication {
 		return "Wao!! Application Deployed successfully in SAP Cloud..";
 	}
 
+	@PostConstruct
+	public void init(){
+		logger.info("Application started");
+
+	}
+
 	public static void main(String[] args) {
+		logger.info("Application executed");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
 
