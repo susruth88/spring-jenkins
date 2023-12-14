f5bba354cd9605f4f9bd33210ccfb562085564ba
susruth88 <susruth88@gmail.com>
2023-12-14 13:33:47 -0500

    Update SpringJenkinsApplicationTests.java

--- src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
@@ -18,6 +18,7 @@ public class SpringJenkinsApplicationTests {
 	@Test
 	public void contextLoads() {
 		logger.info("Test case executing...");
+		logger.info("Test case executing second time...");
 		assertEquals(true, true);
 	}
 
